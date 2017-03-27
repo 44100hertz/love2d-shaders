a2d99daec4c1a3beda3ec42e556333a5112f128d
Sam Pagenkopf <ssaammp@gmail.com>
2017-03-27 16:04:35 -0500

    "identity" shader from wiki

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
 local lg = love.graphics
 
 local img = lg.newImage("testimage.png")
+local shader = lg.newShader("shaders/identity.glsl")
 
 love.load = function ()
    love.window.setMode(img:getDimensions())
+   lg.setShader(shader)
 end
 
 love.draw = function ()
